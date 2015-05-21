b5ed7b7b9896cbfbb23629e30bfe8967a0e4294a
Richard Kho <rkho@me.com>
2015-05-20 17:02:13 -0700

    Cleaned up code. Removed pseudocode and variables used for testing.

--- stockProfits.js
+++ stockProfits.js
@@ -5,31 +5,14 @@
 //    1) A buy and sell must occur. Cannot return undefined/null
 //    2) A negative profit can occur.
 //    3) No short selling. Cannot sell before you buy.
-//    4) No time/space complexity, but optimize for it.
+//    4) Optimize for O(n) Linear Time
 
-var arr = [100, 102, 105, 103, 101, 107, 103, 102, 97, 100] //Expected output = $7 (buy at $100 at arr[0], sell at $107 at arr[5])
-
-var negArr = [100, 98, 95, 92, 91, 90, 89, 85, 81] //Expected output = -$1 (buy at $92 at negArr[3], sell at $91 at negArr[4])
-
-var arr2 = [10, 60, 5, 54, 0, 53] //Expect output = $53 (buy at 0 at arr2[5], sell at $53 at arr[6])
-
-// Naive solution would be to run this in exponential time: Running one for-loop within another to find the highest possible profit.
-  // Exponential time doesn't seem like a very optimized soution, so I'm going to skip it for now.
-
-// The function should accept an array as its parameter.
 var getBestProfit = function(arr) {
-  // It should also keep a 'result' variable handy.
   var result;
   var currentLowest = arr[0];
-    // One way to approach this: We can iterate over the array and find the lowest possible price, and then run a binary search to find its highest sell price
-      // Edge case: This would not work if the array passed in was negArray above, since we would catch $81 at negArr[8] as the lowest.
-    // Another way to approach this: We can iterate through the array, keeping track of the following things:
     for (var i = 1; i < arr.length; i++) {
-      // The profit between the current 'lowest' number and the next number.
-      if (result < 0) {
-        if (result < arr[i] - currentLowest) {
-          result = arr[i] - currentLowest;
-        }
+      if (result < 0 && result < arr[i] - currentLowest) {
+        result = arr[i] - currentLowest;
       }
       if (!result || result < arr[i] - currentLowest) {
         result = arr[i] - currentLowest;
@@ -37,12 +20,6 @@ var getBestProfit = function(arr) {
       if (currentLowest > arr[i]) {
         currentLowest = arr[i];
       }
-        // If the new number is lower than the 'lowest' number, we still set the profit and re-start with this new lowest number.
-        // If the new number is higher than the 'lowest' number, we should place the difference as the current profit.
     }
-  // At the end of this, it should return that result variable
   return result;
 }
-
-result = 50
-currentLowest = 10
