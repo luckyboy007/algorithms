52056576b4f8ddf1c0972388a9ecde95fe2cd7b8
rkho <rkho@me.com>
2015-07-26 19:46:42 -0700

    Finished angryProfessor

--- angryProfessor.js
+++ angryProfessor.js
@@ -10,42 +10,50 @@
   Output: 'YES' if the class gets cancelled, 'NO' if it does not.
 */
 
-
 function angryProfessor(string) {
-  // Split the string into an array by its line break
+  // First, split my string into an array by its line breaks
   var infoArr = string.split('\n');
+  // Grab T, the number of test cases
+  var testCases = parseInt(infoArr[0]);
+  // Store the results in a string
+  var results = '';
 
-  // Grab the number of test cases
-  var testCases = infoArr[0];
+  // Iterate from 0 to testCases
+  for (var i = 1, startValue = 1; i <= testCases; i++) {
+    // Turn the info line into an array, split by the space, and grab the numbers
+    var classInfo = infoArr[startValue].split(' ');
+    console.log('classInfo ' + classInfo);
+    var enrolledStudents = parseInt(classInfo[0]);
+    console.log('enrolledStudents ' + enrolledStudents);
+    var minThreshold = parseInt(classInfo[1]);
+    console.log('minThreshold ' + minThreshold);
 
-  // Object to hold each individual class:
-  var classObj = {};
+    // Grab the attendance
+    var classAttendance = infoArr[(startValue + 1)].split(' ');
+    console.log('classAttendance ' + classAttendance);
 
-  // Place class info into class object
-  for (var i = 1; var j = 0; i < infoArr.length; i++) {
-    classObj[j] = classObj[j] || [];
-    classObj[j].push(infoArr[i]);
-    if (i % 2 === 0) {
-      j++;
-    }
-  }
+    // Count the number of late students
+    var lateStudents = 0;
 
-  // Create a placeholder string for results
-  var result = '';
-
-  // Subroutine to check each test case
-  function subroutine(startValue) {
-    if (startValue === testCases) {
-      console.log(result);
-      return;
-    }
-    var threshold = classObj[startValue][0].split(' ')[1];
-    var attendanceArr = classObj[startValue][1].split(' ');
-
-    attendanceArr.forEach(function(elem) {
-
+    // Iterate over class attendance
+    classAttendance.forEach(function (elem) {
+      console.log('parseInt ' + parseInt(elem));
+      if (parseInt(elem) > 0) {
+        lateStudents++;
+        console.log('lateStudents ' + lateStudents)
+      }
     })
+
+    // If we subtract the number of lateStudents from enrolledStudents and it is greater than the threshold, append 'NO' to results.
+
+    if ((enrolledStudents - lateStudents) >= minThreshold) {
+      results += 'NO\n';
+    } else {
+      results += 'YES\n';
+    }
+    console.log('results ' + results;)
+    startValue += 2;
   }
 
-  subroutine(testCases);
+  console.log(results);
 }
