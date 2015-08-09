06ca455c39f25b5fa49cd3160722e53c25e6e3ee
rkho <rkho@me.com>
2015-08-08 23:14:04 -0700

    Further logic

--- sherlockAndTheBeast.js
+++ sherlockAndTheBeast.js
@@ -36,13 +36,36 @@ function sherlockAndTheBeast(string) {
     var modThree = n % 3;
     var modFive = n % 5;
 
-    var numberOfFives;
+    // Logic needs to be heavily refactored here.
+    // Control flow is only entering the first condition if string = falsy
+    // and if n % 3 returns no remainder. This would mean that we never dive in here
+    // if there is ever a remainder.
+
+    // First, let's grab the number of fives.
+    var numberOfFives = Math.floor(n/5);
     var numberOfThrees;
+
+    // If the number of fives is divisible by 3, we'll continue:
+
+    if (!str && numberOfFives % 3 === 0) {
+      // If the number of fives is exactly N, just return a string of all fives
+      if (numberOfFives === n) {
+        str = buildString(5, n);
+      } else {
+        numberOfThrees = (n - numberOfFives);
+        if (numberOfThrees && numberOfThrees % 5 === 0) {
+          str = buildString(5, numberOfFives) + buildString(3, numberOfThrees)
+        } else {
+          str = -1;
+        }
+      }
+    }
+
+
+
 
     if (!str && modThree === 0) {
       if (modThree % 5 === 0) {
-        numberOfFives = Math.floor(n/5);
-        numberOfThrees = n - numberOfFives;
 
         str = buildString(5, numberOfFives) + buildString(3, numberOfThrees);
       } else {
@@ -51,10 +74,10 @@ function sherlockAndTheBeast(string) {
     } else if (!str && modFive === 0) {
       if (modFive % 3 === 0) {
         // build a string of n length consisting of 3s until there's a remainder, and use the remaining slots for 3s
-        numberOfThress = Math.floor(n/3);
+        numberOfThrees = Math.floor(n/3);
         numberOfFives = n - numberOfThrees;
 
-        str = buildString()
+        str = buildString(3. numberOfThrees)
 
         // EDGE CASE - Will there EVER be a situation other than a string of all threes where threes come before fives?
       } else {
